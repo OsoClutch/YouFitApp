8bf0d33a12e42411a8dc6903de449d7980cb2880
OsoClutch <marcus.knighton.mk@gmail.com>
2026-03-25 14:50:47 -0400

    YouFit Sprint 1 - Luma shell

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return(
+    <div className="luma-container">
+      <h1 className="youfit-title">YOUFIT</h1>
+      <p className="youfit-sub">Holographic AI Try-On</p>
+    </div>
+  )
+}
+
+export default App
